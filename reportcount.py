99e3cb429c4887c43071d201191a3412de27b10a
Sumit sekhar <sumit.leadwinner@gmail.com>
2025-06-09 10:42:31 +0530

    port issue was resolved

--- reportcount.py
+++ reportcount.py
@@ -87,7 +87,6 @@ def initialize_driver(download_dir):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_experimental_option("prefs", {
         "download.default_directory": download_dir,
